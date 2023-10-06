2c37f399e53f2abc065d754e80b4fa8f080f51ab
Yahya Iddris Fatah <123132207+balatwist@users.noreply.github.com>
2023-10-05 21:18:07 -0500

    update

--- Question1/Reverse.java
+++ Question1/Reverse.java
@@ -4,20 +4,20 @@ public class Reverse {
     static int count =0;
     public static void main(String[] args) {
      rev(153568);
-        System.out.println(count);
+
     }
 
     public static void rev(int n){
         if (n<10){
 
             System.out.println(n);
-count++;
+
             return;
         }
 else {
 
-            System.out.println(n%10);
-           ++ count;
+            System.out.print(n%10);
+
             rev(n/10);
 
         }
--- Question2/Counter.java
+++ Question2/Counter.java
@@ -3,7 +3,7 @@ package Question2;
 public class Counter {
     public static void main(String[] args) {
         countDigits(123456789);
-        System.out.println("Total number of digits is  = "+count);
+        System.out.println("\nTotal number of digits is  = "+count);
     }
     static  int count;
      static void countDigits(int num){
@@ -12,7 +12,7 @@ public class Counter {
             return;
         }
         else {
-            System.out.println(num % 10);
+            System.out.print(num % 10);
          count++;
          countDigits(num/10);
         }
